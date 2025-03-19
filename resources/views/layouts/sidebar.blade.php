4100be9ef4c915714d8bdcceaf84dad99b9a3a44
rusmn06 <rusmana.deni06@gmail.com>
2025-03-19 15:39:23 +0800

    deni-19maret2025

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,6 +6,17 @@
             </a>
 
             <!-- Divider -->
+            <hr class="sidebar-divider my-1">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashborad">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">LOGO</div>
+            </a>
+
+            <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
@@ -20,42 +31,28 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Menu Manajemen
             </div>
 
-            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- Nav Item - Kuisener Tenaga Kerja -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
+                <a class="nav-link" href="tenagakerja">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Kuisener Tenaga Kerja</span></a>
             </li>
 
-            <!-- Nav Item - Utilities Collapse Menu -->
+            <!-- Nav Item - Kuisener Jamsos -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
-                    </div>
-                </div>
+                <a class="nav-link" href="jamsos">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Kuisener Jamsos</span></a>
+            </li>
+
+            <!-- Nav Item - Kuisener Difabel Rentan -->
+            <li class="nav-item">
+                <a class="nav-link" href="difabelrentan">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Kuisener Difabel Rentan</span></a>
             </li>
 
             <!-- Divider -->
@@ -80,25 +77,8 @@
                         <a class="collapse-item" href="register.html">Register</a>
                         <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
                         <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
                     </div>
                 </div>
-            </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
             </li>
 
             <!-- Divider -->
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,18 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('layouts.app');
 });
+
+// menu tenaga kerja
+Route::get('/tenagakerja', function () {
+    return view('pages.tenagakerja.index');
+});
+
+// menu jamsos
+Route::get('/jamsos', function () {
+    return view('pages.jamsos.index');
+});
+
+// menu difabel rentan
+Route::get('/difabelrentan', function () {
+    return view('pages.difabelrentan.index');
+});
