25b483a41ec17f3f7855b79f4b2745434b0afcb7
rusmn06 <rusmana.deni06@gmail.com>
2025-05-14 14:43:23 +0800

    edit table

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -18,9 +18,6 @@
     </div>
 
     <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Daftar Akun User</h6>
-        </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table id="akunTable" class="table table-bordered" width="100%" cellspacing="0">
--- resources/views/pages/tenagakerja/listrt/1.blade.php
+++ resources/views/pages/tenagakerja/listrt/1.blade.php
@@ -20,9 +20,6 @@
 
     <!-- DataTables -->
     <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Daftar Form Tenaga Kerja</h6>
-        </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table id="dataTable" class="table table-bordered" width="100%" cellspacing="0">
